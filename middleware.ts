2792f58c048ba892584b411fc7af1725790cb38c
Kshitiz Kumar <kshitijk83@gmail.com>
2024-03-18 16:25:54 +0530

    updates

--- middleware.ts
+++ middleware.ts
@@ -1,19 +1,16 @@
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-  const isAuth = request.cookies.get("token")?.value;
-
-  if (isAuth && !request.nextUrl.pathname.startsWith("/dashboard")) {
-    return Response.redirect(new URL("/dashboard", request.url));
-  }
-
-  if (!isAuth && request.nextUrl.pathname.startsWith("/dashboard")) {
-    return Response.redirect(new URL("/login", request.url));
-  }
-
-  if (request.nextUrl.pathname === "/") {
-    return Response.redirect(new URL("/login", request.url));
-  }
+  // const isAuth = request.cookies.get("token")?.value;
+  // if (isAuth && !request.nextUrl.pathname.startsWith("/dashboard")) {
+  // return Response.redirect(new URL("/dashboard", request.url));
+  // }
+  // if (!isAuth && request.nextUrl.pathname.startsWith("/dashboard")) {
+  //   return Response.redirect(new URL("/login", request.url));
+  // }
+  // if (request.nextUrl.pathname === "/") {
+  //   return Response.redirect(new URL("/login", request.url));
+  // }
 }
 
 export const config = {
